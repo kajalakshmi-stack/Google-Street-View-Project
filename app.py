bc7158f820a3f64d131d520df8f0c89cd5ce50b7
Kaja Lakshmi-AI <lakshmikaja47@gmail.com>
2026-06-18 17:09:15 +0530

    Google Street View Computer Vision Project
    README.md
    GL.ipynb
    app.py
    requirements.txt

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load trained model
+model = tf.keras.models.load_model(
+    "google_street_view_model.h5"
+)
+
+st.title("Google Street View Recognition System")
+
+uploaded_file = st.file_uploader(
+    "Upload an Image",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+
+    st.image(
+        image,
+        caption="Uploaded Image",
+        use_container_width=True
+    )
+
+    img = np.array(image)
+
+    img = cv2.resize(
+        img,
+        (128,128)
+    )
+
+    img = img / 255.0
+
+    img = np.expand_dims(
+        img,
+        axis=0
+    )
+
+    prediction = model.predict(img)
+
+    st.subheader("Prediction Result")
+
+    st.write(prediction)
